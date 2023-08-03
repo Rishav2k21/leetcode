5e28b21eea65e2c357d81231b68d692a9a4977b1
Rishav Anand <98807848+Rishav2k21@users.noreply.github.com>
2023-08-03 18:37:45 +0530

    Time: 9 ms (68.09%), Space: 7.9 MB (99.28%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,36 +1,37 @@
-// class Solution {
-// public:
-//     string convert(string s, int numRows) {
-//         int n=s.size()-1;
-//         string st;
-//         int jum=numRows*2-2;
-//         for(int i=0;i<numRows;i++){
-//             for(int j=i;j<=n;j+=jum){
-//                 st.push_back(s[j]);
-//                 int k=j+jum-2*i;
-//                 if(k<n&& j!=k && k!=j+jum){
-//                     st.push_back(s[k]);
-//                 }
-//             }
-        
-//         }
-//         return st;
-//     }
-// };
 class Solution {
 public:
     string convert(string s, int numRows) {
-        if(numRows == 1) return s;
-        int n = s.size();
-        int jump = 2*numRows - 2;
-        string ans;
-        for(int i=0; i<numRows; i++){
-            for(int j=i; j<n; j+=jump){
-                int k = j + jump - 2*i ;
-                ans.push_back(s[j]);
-                if(k<n && k!=j && k!=j+jump) ans.push_back(s[k]);
+if(numRows==1)return s;
+        int n=s.size();
+        string st;
+        int jum=2*numRows-2;
+        for(int i=0;i<numRows;i++){
+            for(int j=i;j<n;j+=jum){
+                st.push_back(s[j]);
+                int k=j+jum-2*i;
+                if(k<n&& k!=j && k!=j+jum){
+                    st.push_back(s[k]);
+                }
             }
+        
         }
-        return ans;
+        return st;
     }
 };
+// class Solution {
+// public:
+//     string convert(string s, int numRows) {
+//         if(numRows == 1) return s;
+//         int n = s.size();
+//         int jump = 2*numRows - 2;
+//         string ans;
+//         for(int i=0; i<numRows; i++){
+//             for(int j=i; j<n; j+=jump){
+//                 int k = j + jump - 2*i ;
+//                 ans.push_back(s[j]);
+//                 if(k<n && k!=j && k!=j+jump) ans.push_back(s[k]);
+//             }
+//         }
+//         return ans;
+//     }
+// };
