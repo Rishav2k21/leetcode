e97e8bbc36222c7dae6d0bc9a321ffe03b1fd0b1
Rishav Anand <98807848+Rishav2k21@users.noreply.github.com>
2023-09-20 04:02:27 +0530

    Added solution - LeetHub

---
+++ Rotate Bits - GFG/rotate-bits.cpp
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+        vector <int> rotate (int n, int d)
+        {
+              d = d % 16;
+            int right = (n >> d) + (((1 << d) - 1) & n ) * (1 << (16 - d));
+            int left = ((n << d) + ((n << d) >> 16)) & ((1 << 16) - 1);
+            
+            return {left, right};
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        
+        int n, d; cin >> n >> d;
+        Solution ob;
+        vector <int> res = ob.rotate (n, d);
+        cout << res[0] << endl << res[1] << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
