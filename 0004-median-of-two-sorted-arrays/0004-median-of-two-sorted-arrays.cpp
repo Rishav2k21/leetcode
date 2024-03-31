f8c500943c4f3d561594930b6cbd58dc687a1a62
Rishav Anand <98807848+Rishav2k21@users.noreply.github.com>
2024-03-31 18:03:53 +0530

    Time: 36 ms (13.57%), Space: 94.3 MB (91.30%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -6,17 +6,14 @@ public:
          if (n1 > n2)
             return findMedianSortedArrays(nums2, nums1);
         int n=n1+n2;
-        int low=0;
+        int start=0;
         int left=(n+1)/2;
-        int high=n1;
-        int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
-            while(low<=high){
-            int mid1 = (low + high) >> 1; // Calculate mid index for nums1
-            int mid2 = left - mid1; // Calculate mid index for nums2
-            
-            int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
-            
-            // Determine values of l1, l2, r1, and r2
+        int end=n1;
+       
+            while(start<=end){
+                int mid1=(start+end)>>1;
+                int mid2=left-mid1;
+                int l1 = INT_MIN, l2 = INT_MIN, r1 = INT_MAX, r2 = INT_MAX;
             if (mid1 < n1)
                 r1 = nums1[mid1];
             if (mid2 < n2)
@@ -25,23 +22,20 @@ public:
                 l1 = nums1[mid1 - 1];
             if (mid2 - 1 >= 0)
                 l2 = nums2[mid2 - 1];
-            
-            if (l1 <= r2 && l2 <= r1) {
-                // The partition is correct, we found the median
-                if (n % 2 == 1)
+                
+                
+            if(l1<=r2 && l2<=r1){
+                 if (n % 2 == 1)
                     return max(l1, l2);
                 else
                     return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
             }
-            else if (l1 > r2) {
-                // Move towards the left side of nums1
-                high = mid1 - 1;
+            else if(l1>r2){
+                end=mid1-1;
             }
-            else {
-                // Move towards the right side of nums1
-                low = mid1 + 1;
-            }
-                
+                else{
+                    start=mid1+1;
+                }
             }
         return 0;
             }
