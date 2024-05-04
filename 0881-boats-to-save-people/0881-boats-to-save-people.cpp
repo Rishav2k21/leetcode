a08b043cc7cdf4deec76bafae683cb7216b6a832
Rishav Anand <98807848+Rishav2k21@users.noreply.github.com>
2024-05-04 10:52:29 +0530

    Time: 51 ms (86.84%), Space: 45.5 MB (8.97%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -3,7 +3,8 @@ public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
         int k=limit;
-        int count=0,s=0;
+        int count=0;
+        int s=0;
         int e=people.size()-1;
         while(s<=e){ if(people[s]+people[e--]<=limit)s++; 
                    count++;}
